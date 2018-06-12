930ffbbde8e8199013b70d52fa78f1ea72d5070a
AdamChrimes <iChrimes@live.co.uk>
2018-06-12 12:36:12 +0100

    :camera_flash: backstopjs initial setup

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,80 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_NHSUK_redesign_0_document_0_phone.png",
+        "test": "../bitmaps_test/20180612-120058/backstop_default_NHSUK_redesign_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_NHSUK_redesign_0_document_0_phone.png",
+        "label": "NHS.UK redesign",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:3000",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_NHSUK_redesign_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20180612-120058/backstop_default_NHSUK_redesign_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_NHSUK_redesign_0_document_1_tablet.png",
+        "label": "NHS.UK redesign",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:3000",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 89
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_NHSUK_redesign_0_document_2_laptop.png",
+        "test": "../bitmaps_test/20180612-120058/backstop_default_NHSUK_redesign_0_document_2_laptop.png",
+        "selector": "document",
+        "fileName": "backstop_default_NHSUK_redesign_0_document_2_laptop.png",
+        "label": "NHS.UK redesign",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:3000",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "laptop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 116
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "backstop_default"
+});
